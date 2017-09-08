04599c770543508463ecfc3794f573f23ea10bea
Zach Cook <zach@zachcookhustles.com>
2017-09-08 16:25:19 -0500

    Basic routing hooked up.

--- app/routes.php
+++ app/routes.php
@@ -24,27 +24,70 @@
     // Docs link to 'groups' doc info - https://www.slimframework.com/docs/objects/router.html#route-groups
     $app->group('/', function () {
 
+        $this->get('', function (Request $request, Response $response, $args) {
+
+            $vars = [
+                'page' => [
+                'title' => 'Brand Builders',
+                'description' => 'Best apparel company in Austin'
+                ],
+            ];
+
+            return $this->view->render($response, 'home.twig', $vars);    
+        });
+        $this->get('index', function (Request $request, Response $response, $args) {
+
+            $vars = [
+                'page' => [
+                'title' => 'Brand Builders',
+                'description' => 'Best apparel company in Austin'
+                ],
+            ];
+
+            return $this->view->render($response, 'home.twig', $vars);    
+        });
+
+        $this->get('products', function (Request $request, Response $response, $args) {
+
+            $vars = [
+                'page' => [
+                'title' => 'Brand Builders',
+                'description' => 'Best apparel company in Austin'
+                ],
+            ];
+            return $this->view->render($response, 'products.twig', $vars);    
+        });
+
+        $this->get('services', function (Request $request, Response $response, $args) {
+
+            $vars = [
+                'page' => [
+                'title' => 'Brand Builders',
+                'description' => 'Best apparel company in Austin'
+                ],
+            ];
+            return $this->view->render($response, 'services.twig', $vars);    
+        });
 
         $this->get('designs', function (Request $request, Response $response, $args) {
 
             $vars = [
                 'page' => [
-                'title' => 'Products - Brand Builders',
-                'description' => 'We offer all of these products!'
+                'title' => 'Brand Builders',
+                'description' => 'Best apparel company in Austin'
                 ],
             ];
             return $this->view->render($response, 'designs.twig', $vars);    
         });
 
-        $this->get('', function (Request $request, Response $response, $args) {
+        $this->get('quote-request', function (Request $request, Response $response, $args) {
 
             $vars = [
                 'page' => [
-                'title' => 'Products - Brand Builders',
-                'description' => 'We offer all of these products!'
+                'title' => 'Brand Builders',
+                'description' => 'Best apparel company in Austin'
                 ],
             ];
-
-            return $this->view->render($response, 'home.twig', $vars);    
+            return $this->view->render($response, 'quote-request.twig', $vars);    
         });
     });
